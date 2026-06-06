2c4e3ba9f9508da71490f9a4a1689d60f46b6545
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-06 18:29:59 +0530

    Add fstrings.py

--- fstrings.py
+++ fstrings.py
@@ -8,3 +8,4 @@ Letter = f''' Dear {name}
 You are selected for the job. 
 {date} '''
 print(Letter)
+# Strings are immutable in Python, which means that once a string is created, it cannot be changed. However, you can create a new string by concatenating or modifying the existing string.
