bac3bee475097fc2cf28c3217730c7d777b52756
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-06 18:02:29 +0530

    Add strfunctions.py

---
+++ strfunctions.py
@@ -0,0 +1,10 @@
+name = "devansh"
+
+print(len(name))
+print(name.endswith("ansh"))
+print(name.startswith("G"))
+print(name.capitalize())
+index = name.find("sh")
+print(index)
+newname = name.replace("dev", "DEV")
+print(newname)
