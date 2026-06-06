111d2691805145ad1df08f2c107ccf5bb531f313
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-06 18:23:37 +0530

    Add fstrings.py

---
+++ fstrings.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print(f"Hello Good Evening, {name}")
+
+date = input("Enter the date: ")
+
+Letter = f''' Dear {name}
+You are selected for the job. 
+{date} '''
+print(Letter)
