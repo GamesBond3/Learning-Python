09071aa5fb43ea15e75eefc84b85764b059032bd
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-06 16:35:05 +0530

    typecastingandinput.py

--- typecastingandinput.py
+++ typecastingandinput.py
@@ -1,5 +1,7 @@
-a = 21
-c = float(a)
-b = type(a)
-print(a)
-print(c)
+a = input("Enter a number: ")
+b = input("Enter second number: ")
+print("Sum is : ", a + b)
+
+c = int(input("Enter a number: "))
+d = int(input("Enter second number: "))
+print("Sum is : ", c + d)
