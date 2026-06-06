7bb4de08d9eec4ed431a3967309595593317ff15
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-07 00:59:34 +0530

    Concatenation

---
+++ concatenation.py
@@ -0,0 +1,9 @@
+Marks = []
+m1 = int(input("Enter marks: "))
+Marks.append(m1)
+m2 = int(input("Enter marks: "))
+Marks.append(m2)
+m3 = int(input("Enter marks: "))
+Marks.append(m3)
+Marks.sort()
+print(Marks)
