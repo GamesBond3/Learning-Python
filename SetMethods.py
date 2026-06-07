4483e04b8111c8a5956eb63ffe534831207eeeaa
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-07 23:49:10 +0530

    Sets

---
+++ SetMethods.py
@@ -0,0 +1,10 @@
+s = {"Devansh", 1, 21, 19, 5, 5, 5} #This is a set
+e = set() #This is an empty set
+d = {} #This is an empty dictionary
+print(s)
+s.add("Anshuman")
+print(s)
+s.remove(5)
+print(s, len(s))
+e.add(1)
+e.add("Devansh") 
