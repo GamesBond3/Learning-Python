4880928b1d24e875466d2045b10ddcb066d2bedc
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-10 17:41:32 +0530

    Break and Continue

---
+++ for3.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    if (i==6):
+        break #Exit the loop immediately
+    print(i)
+
+for i in range(11):
+    if ((i==5)or(i==9)):
+        continue #Skip this iteration
+    print(i)
