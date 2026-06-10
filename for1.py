61c41c5d432ffa82fb08593a157d56371f5bc7a4
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-10 17:30:45 +0530

    For Loops

---
+++ for1.py
@@ -0,0 +1,21 @@
+# for i in range(4):
+#     print(i)
+
+#for loop with skipping numbers
+'''
+for i in range(0, 10, 2):
+    print(i)
+'''
+
+#for loop for tuples
+'''
+t = (1, 2, 3, 4, 5)
+for i in t:
+    print(i)
+'''
+# for loop for strings
+''' 
+str = "Devansh"
+for i in str:
+    print(i)
+'''
