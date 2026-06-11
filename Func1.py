d5e4741400f6294666b7c709a026f3116edd811c
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-11 18:48:34 +0530

    basic function

---
+++ Func1.py
@@ -0,0 +1,8 @@
+def avg():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    num3 = int(input("Enter the third number: "))
+    average = (num1 + num2 + num3) / 3
+    print(f"The average of {num1}, {num2} and {num3} is {average}")
+
+avg()
