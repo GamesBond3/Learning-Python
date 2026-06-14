00dcc63f8db019a105866b595953f090688783ef
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-14 18:46:33 +0530

    comparing and updating file

--- fileprob2.py
+++ fileprob2.py
@@ -4,7 +4,7 @@ def game():
     print("Game has begun")
     score = random.randint(1,100)
     #fecthing score
-    with open ("fileprob3hiscore.txt")as f:
+    with open ("fileprob2hiscore.txt")as f:
         hiscore = f.read()
         if (hiscore != ""):
             hiscore = int(hiscore)
@@ -14,7 +14,7 @@ def game():
     print(f"Your score = {score}")
     if(score>hiscore):
         #writing in hiscore file
-        with open("fileprob3hiscore.txt", "w") as f:
+        with open("fileprob2hiscore.txt", "w") as f:
             f.write(str(score))
 
     return score
