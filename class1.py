51e6ba783c7788a64b98a241fda3879bbc10b8f0
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-15 23:49:31 +0530

    Classes

---
+++ class1.py
@@ -0,0 +1,8 @@
+class Employee:
+    language = "English"
+    Salary = "$120K"
+    #These are class attributes
+Devansh = Employee()
+Devansh.name = "Devansh" #this is instance/object attribute
+print(Devansh.name, "\n", Devansh.language, Devansh.Salary)
+
