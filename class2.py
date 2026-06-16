961d858f28190bd0790b4860dd0db0f7a8980422
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-16 17:55:36 +0530

    Use of "self" argument

---
+++ class2.py
@@ -0,0 +1,13 @@
+class Employee:
+    language = "English"
+    Salary = "$120K"
+    #These are class attributes
+    def getInfo(self):
+        print(f"The language is {self.language} and Salary is {self.Salary}")
+Devansh = Employee()
+Devansh.name = "Devansh" #this is instance/object attribute
+Devansh.language = "Python"
+print(Devansh.name, "\n", Devansh.language, Devansh.Salary)
+
+Devansh.getInfo() # this converts into Employee.getInfo(Devansh) this means there is an argument so we use Self
+# Employee.getInfo(Devansh) this can also be used
