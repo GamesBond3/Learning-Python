8a4cf56772413ce126add85cae3b7cc959ef91b6
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-21 18:27:36 +0530

    using try with finally

---
+++ try_finally.py
@@ -0,0 +1,14 @@
+def main():
+    try:
+        a = int(input("Enter Number: "))
+        print (a)
+        return
+
+    except Exception as e:
+        print(e)
+        return
+    
+    finally:            #Finally works even if the func fails to execute
+        print("Working")
+    
+main()
