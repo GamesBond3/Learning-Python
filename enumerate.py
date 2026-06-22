4d273505c3ed6d0a89223aec57e614bb64e020a4
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-22 18:16:42 +0530

    enumerate function

---
+++ enumerate.py
@@ -0,0 +1,14 @@
+l = [1,4,5,6]
+
+
+# index = 0 
+# for item in l:
+#     print(f"At index {index} is {item}")
+#     index +=1
+
+
+'''         Alterante Method            '''
+
+
+for index, item in enumerate(l):
+    print(f"At index {index} is {item}")
