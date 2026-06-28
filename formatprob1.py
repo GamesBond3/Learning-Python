560c7ea2c5d6eb05f4c5c220699d3553823d0f83
GamesBond3 <devanshpateriya9115@gmail.com>
2026-06-28 20:16:31 +0530

    probelms on format

---
+++ formatprob1.py
@@ -0,0 +1,6 @@
+Name = input("Enter Name: ")
+Marks = int(input("Enter Marks: "))
+Phone = int(input("Enter Phone: "))
+
+S = "{} has scored {} and his phone number is {}".format(Name, Marks, Phone)
+print(S)
